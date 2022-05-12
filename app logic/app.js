485dbbdc81fbbcdb462d2f0f130bf4fa049725f6
SKLC1 <segevklein2000@gmail.com>
2022-05-12 23:42:49 +0300

    saving progress

--- app logic/app.js
+++ app logic/app.js
@@ -1,9 +1,22 @@
 import { getData } from "./data.js";
 import { createChart } from "./chart.js";
 
+
 async function createApp() {
   const data = await getData()
   // console.log(data);
-  createChart(data, 'Africa')
+  function handleButton (e) {
+    const continent = e.currentTarget.innerText;
+    createChart(data, continent)
+  }
+  // 
+  function appendButtons() { 
+    const btns = document.querySelectorAll('.btn')
+    const btnsArr = [...btns]
+    btnsArr.forEach(btn =>{
+      btn.addEventListener('click', handleButton)
+     })
+  }
+  appendButtons()
 }
 createApp()
--- app logic/chart.js
+++ app logic/chart.js
@@ -1,41 +1,43 @@
 
 const ctx = document.getElementById('myChart').getContext('2d');
 
+
 export async function createChart(dataArr, continent) {
-  console.log(dataArr);
-  console.log();
-  const myChart = new Chart(ctx, {
+  
+  const covidStatsChart = new Chart(ctx, {
     type: 'bar',
     data: {
       labels: dataArr[continent].map(country=> {return country.name}),
       datasets: [{
-          label: '# of Votes',
-          data: dataArr[continent].map(country=> {return country.latest_data['confirmed']}),
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
+        label: '# of Votes',
+        data: dataArr[continent].map(country=> {return country.latest_data['confirmed']}),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
             'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-      }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
       }
-    }
+    });
   }
- });
-}
+
+  
